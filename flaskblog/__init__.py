bde471c3f11706f5222210c04d743cda8b47336d
Timofei Yatagandjidi <timofei_kahharov@mail.ru>
2022-07-10 14:46:53 +0600

    First commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '35e6b1bc0138c8ba6151ea18f4d8d983'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from flaskblog import routs
